f9498967d232929d5cf6f6b18b4804320b133ff3
Gabriel <g.romeu1910@gmail.com>
2024-12-12 11:16:50 -0500

    Initial commit

---
+++ operations.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const port = 3000;
+
+// allow requests from anywhere
+app.use(cors());
+
+// define our api route
+// add, sub, mult, div, pwr, root, sqrt
+app.get("/div", (req, res) => {
+  const a = parseFloat(req.query.a);
+  const b = parseFloat(req.query.b);
+
+  if (b === 0) {
+	return res.status(400).json({ error: "Division by zero is not allowed!" })
+  }
+
+  const result = a / b;
+  const remainder = a % b;
+  return res.json({ result, remainder })
+});
+
+app.listen(port, () => {console.log("Server running on " + port)});

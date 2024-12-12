4578df0eb94240e1928c35c498444845e6199deb
Gabriel <g.romeu1910@gmail.com>
2024-12-12 12:00:38 -0500

    Initial commit

--- operations.js
+++ operations.js
@@ -21,4 +21,6 @@ app.get("/div", (req, res) => {
   return res.json({ result, remainder })
 });
 
-app.listen(port, () => {console.log("Server running on " + port)});
+// app.listen(port, () => {
+//  console.log("Server running on " + port)
+// });
